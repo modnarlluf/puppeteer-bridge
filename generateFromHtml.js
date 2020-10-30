faa7438ab8687a0c3f95688110d1468a0b4fc7bb
Brice Sigura <brice.sigura@kelips.fr>
2020-10-30 21:42:26 +0100

    Add big try catch

--- generateFromHtml.js
+++ generateFromHtml.js
@@ -5,7 +5,6 @@ const generateFromFile = require('./generateFromFile');
 async function generateFromHtml(options) {
     const tmpHtmlFile = tmp.fileSync({postfix: '.html'});
     const tmpPdfFile = tmp.fileSync({postfix: '.pdf'});
-
     fs.writeFileSync(tmpHtmlFile.name, options.html);
     await generateFromFile(tmpHtmlFile.name, {...options, pdfOutputPath: tmpPdfFile.name});
 
